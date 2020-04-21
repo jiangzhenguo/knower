55fd3d0e3cdfcb116a5e0568e7911e44dc00473d
jiangzhenguo <jiangzhenguo@babytree-inc.com>
2020-04-21 15:03:40 +0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Player from 'aliplayer-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const config = {
+    source: "//player.alicdn.com/video/aliyunmedia.mp4",
+    width: "100%",
+    height: "100%",
+    autoplay: true,
+    isLive: true,
+    rePlay: false,
+    playsinline: true,
+    preload: true,
+    controlBarVisibility: "hover",
+    useH5Prism: true,
+    x5_type: 'h5',
+    skinLayout: [
+        {
+            "name": "bigPlayButton",
+            "align": "blabs",
+            "x": 30,
+            "y": 80
+        },
+        {
+            "name": "errorDisplay",
+            "align": "tlabs",
+            "x": 0,
+            "y": 0
+        },
+        {
+            "name": "infoDisplay"
+        },
+        {
+            "name": "controlBar",
+            "align": "blabs",
+            "x": 0,
+            "y": 0,
+            "children": []
+        }
+    ],
+    components: [
+        {
+            name: "RateComponent",
+            type: Player.components.RateComponent,
+        }
+    ]
+};
 
-export default App;
+ export default class App extends Component{
+     constructor(props, context) {
+         super(props, context);
+         this.state = {
+             instance: null,
+         }
+     }
+
+     componentDidMount() {
+
+     }
+
+     render() {
+      return (
+          <div className='App'>
+              <Player
+                  config={config}
+                  onGetInstance={instance => this.setState({ instance })}
+                  />
+          </div>
+      )
+    }
+ }
+
